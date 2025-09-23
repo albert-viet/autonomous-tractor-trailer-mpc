09021991e890818c47ef80b28231f14d07f91927
Viet-Quoc Ha <haquocviet260798@gmail.com>
2025-09-23 19:23:16 +0700

    add more trailers and modify the linearised model

--- linear_mpc.m
+++ linear_mpc.m
@@ -2,61 +2,70 @@
 clear; close all; clc;
 
 %% --- model parameters ---
-tau = 0.3; lr1 = 2.9; lt1 = 1.8; m0 = 0.8;  % trailer-1
-lt2 = 2.0;                                  % trailer-2
+SCENARIO = "SINUSOIDAL";
+tau = 0.3; lr1 = 2.9; lt1 = 1.8; m0 = 0.8;            % trailer-1
+lt2 = 2.0; lbar=0.4;                                  % trailer-2
 %% sampling
 dt = 0.03; umax = 0.52; umin = -umax;
 %% MPC tuning
 Np = 50;   % prediction horizon
 % Qx = diag([9.3706, 1.2718, 0.10021]);   % state weights (same ordering e_y,e_psi,e_psi_t)
 % Ru = 0.3821;                   % control move weight (on u_dev)
-Qx = diag([10, 5, 0.1]);
-Ru = 0.5;
+% Qx = diag([10, 5, 0.1]);
+Ru = 0.3;
+% add one trailer
+Qx = diag([1, 1, 5, 5]);             % extended states (e_y, e_psi, e_psi_t, e_psi_t_ex)
 regularization = 1e-6;
 u_prev = 0.0;
 max_rate = 1.2;            % maximum steering rate (rad/s)
 %% Reference generation
 timeSpan = 60; timeStep = 0.005;
 t_ref = (0:timeStep:timeSpan)';
-% x_a_r = t_ref * 2 * 20 / timeSpan;
-% Rparam = 20; alpha_l = 12; c = 10;
-% y_a_r = c * (exp(-log(2) * (x_a_r./Rparam).^alpha_l) - 1);
 
-% Allocate reference
-x_a_r = zeros(size(t_ref));
-y_a_r = zeros(size(t_ref));
-psi_ref = zeros(size(t_ref));
-v_ref = zeros(size(t_ref));
+if SCENARIO == "LANE_CHANGE"
+    disp('Running lane change scenario ...')
+    x_a_r = t_ref * 2 * 20 / timeSpan;
+    Rparam = 20; alpha_l = 12; c = 10;
+    y_a_r = c * (exp(-log(2) * (x_a_r./Rparam).^alpha_l) - 1);
 
-% ---- Phase 1: t = [0,3] ----
-idx1 = t_ref <= 3;
-t1 = t_ref(idx1);
-vr1 = 0.1 + 0.9 * sin(pi * t1 / 6);           % longitudinal velocity
-x1 = cumtrapz(t1, vr1);                        % integrate to get x
-y1 = zeros(size(t1));
-psi1 = zeros(size(t1));                        % heading constant 0
-
-x_a_r(idx1) = x1;
-y_a_r(idx1) = y1;
-psi_ref(idx1) = psi1;
-v_ref(idx1) = vr1;
-
-% ---- Phase 2: t = [3,43] ----
-idx2 = t_ref > 3;
-t2 = t_ref(idx2);
-x2 = t2 - 1;
-y2 = 5 * sin(pi/20 * (t2 - 3) + 1.5*pi) + 5;
-
-dx2 = gradient(x2, timeStep);
-dy2 = gradient(y2, timeStep);
-
-psi2 = atan2(dy2, dx2);
-vr2 = sqrt(dx2.^2 + dy2.^2);
-
-x_a_r(idx2) = x2;
-y_a_r(idx2) = y2;
-psi_ref(idx2) = psi2;
-v_ref(idx2) = vr2;
+elseif SCENARIO == "SINUSOIDAL"
+    disp('Running sinusoidal scenario ...')
+    % Allocate reference
+    x_a_r = zeros(size(t_ref));
+    y_a_r = zeros(size(t_ref));
+    psi_ref = zeros(size(t_ref));
+    v_ref = zeros(size(t_ref));
+    
+    % ---- Phase 1: t = [0,3] ----
+    idx1 = t_ref <= 3;
+    t1 = t_ref(idx1);
+    vr1 = 0.1 + 0.9 * sin(pi * t1 / 6);           % longitudinal velocity
+    x1 = cumtrapz(t1, vr1);                        % integrate to get x
+    y1 = zeros(size(t1));
+    psi1 = zeros(size(t1));                        % heading constant 0
+    
+    x_a_r(idx1) = x1;
+    y_a_r(idx1) = y1;
+    psi_ref(idx1) = psi1;
+    v_ref(idx1) = vr1;
+    
+    % ---- Phase 2: t = [3,43] ----
+    idx2 = t_ref > 3;
+    t2 = t_ref(idx2);
+    x2 = t2 - 1;
+    y2 = 5 * sin(pi/20 * (t2 - 3) + 1.5*pi) + 5;
+    
+    dx2 = gradient(x2, timeStep);
+    dy2 = gradient(y2, timeStep);
+    
+    psi2 = atan2(dy2, dx2);
+    vr2 = sqrt(dx2.^2 + dy2.^2);
+    
+    x_a_r(idx2) = x2;
+    y_a_r(idx2) = y2;
+    psi_ref(idx2) = psi2;
+    v_ref(idx2) = vr2;
+end
 %% calculate reference profile
 
 dx = gradient(x_a_r, t_ref); ddx = gradient(dx, t_ref);
@@ -85,14 +94,21 @@ end
 % tractor states
 x_act = zeros(Tsim+1,1); y_act = zeros(Tsim+1,1);
 psi_act = zeros(Tsim+1,1); psi_t_act = zeros(Tsim+1,1);
+psi_t_t_act=zeros(Tsim+1,1);
 delta_act = zeros(Tsim+1,1);
 % trailer states
-x_t_act = zeros(Tsim+1,1); y_t_act = zeros(Tsim+1,1);
+x_t_act = zeros(Tsim+1,1); y_t_act = zeros(Tsim+1,1);       % trailer-1
+x_t_t_act = zeros(Tsim+1,1); y_t_t_act = zeros(Tsim+1,1);   % trailer-2
 
 % tractor and trailer initial pose
-x_act(1)=0.2; y_act(1)=0.4; psi_act(1)=0.0; psi_t_act(1)=0.0; delta_act(1)=0.0;
-x_t_act(1) = x_act(1) - lt1*cos(psi_t_act(1)) - m0*cos(psi_act(1));
-y_t_act(1) = y_act(1) - lt1*sin(psi_t_act(1)) - m0*sin(psi_act(1));
+x_act(1)=3.0; y_act(1)=1.0; psi_act(1)=0.0; psi_t_act(1)=0.1; delta_act(1)=0.0;
+psi_t_t_act(1)=0.0;
+% trailer-1's pose
+x_t_act(1) = x_act(1) - (lt1)*cos(psi_t_act(1)) - m0*cos(psi_act(1));
+y_t_act(1) = y_act(1) - (lt1)*sin(psi_t_act(1)) - m0*sin(psi_act(1));
+% trailer-2's pose
+x_t_t_act(1) = x_t_act(1) - lt2*cos(psi_t_t_act(1)) - lt1*cos(psi_t_act(1));
+y_t_t_act(1) = y_t_act(1) - lt2*sin(psi_t_t_act(1)) - lt1*sin(psi_t_act(1));
 
 % initial error (Frenet)
 % dx0 = x_act(1) - x_ref(1); dy0 = y_act(1) - y_ref(1);
@@ -101,34 +117,41 @@ y_t_act(1) = y_act(1) - lt1*sin(psi_t_act(1)) - m0*sin(psi_act(1));
 % x_err = [ e_y0; wrapToPi(psi_act(1)-psi_ref_i(1)); wrapToPi(psi_t_act(1)-psi_ref_i(1)); delta_act(1)];
 [xproj_tr, yproj_tr, psi_ref_tr, k_tr_init, delta_r_tr_init, idx_tr_init] = projectRefPoint(x_act(1), y_act(1), x_ref, y_ref, psi_ref_i, k_profile, delta_r_profile);
 [xproj_tl, yproj_tl, psi_ref_tl, k_tl_init, delta_r_tl_init, idx_tl_init] = projectRefPoint(x_t_act(1), y_t_act(1), x_ref, y_ref, psi_ref_i, k_profile, delta_r_profile);
+[xproj_tl_t, yproj_tl_t, psi_ref_tl_t, k_tl_t_init, delta_r_tl_t_init, idx_tl_t_init] = projectRefPoint(x_t_t_act(1), y_t_t_act(1), x_ref, y_ref, psi_ref_i, k_profile, delta_r_profile);
 
 dx0 = x_act(1) - xproj_tr; dy0 = y_act(1) - yproj_tr;
 e_y0 = -sin(psi_ref_tr)*dx0 + cos(psi_ref_tr)*dy0;
 e_psi0 = wrapToPi(psi_act(1) - psi_ref_tr);
 e_psi_t0 = wrapToPi(psi_t_act(1) - psi_ref_tl);
+e_psi_t_t0 = wrapToPi(psi_t_t_act(1) - psi_ref_tl_t);
 
-x_err = [e_y0; e_psi0; e_psi_t0; delta_act(1)];
+x_err = [e_y0; e_psi0; e_psi_t0; e_psi_t_t0; delta_act(1)];
 % keep previous projection indices to accelerate next searches (optional)
 last_idx_tr = idx_tr_init;
 last_idx_tl = idx_tl_init;
+last_idx_t1_t = idx_tl_t_init;
 
-history_err = zeros(4, Tsim+1); history_err(:,1) = x_err;
+history_err = zeros(5, Tsim+1); history_err(:,1) = x_err;
 u_hist = zeros(Tsim,1);
 
 %% Precompute block weights
-nx = 4; nu = 1; ny = 3; % output: e_y, e_psi, e_psi_t
+nx = 5; nu = 1; ny = 4; % output: e_y, e_psi, e_psi_t, e_psi_t_t (extend)
 Qbar = kron(eye(Np), Qx);
 Rbar = kron(eye(Np), Ru);
 
 %% Main MPC loop
+beta1_hist = zeros(Tsim,1);
+beta2_hist = zeros(Tsim,1);
 for k = 1:Tsim
     % project tractor & trailer current positions to reference
     [xproj_tr, yproj_tr, psi_ref_tr, k_truck, delta_r_truck, last_idx_tr] = projectRefPoint(x_act(k), y_act(k), x_ref, y_ref, psi_ref_i, k_profile, delta_r_profile);
     [xproj_tl, yproj_tl, psi_ref_tl, k_trailer, delta_r_tl, last_idx_tl] = projectRefPoint(x_t_act(k), y_t_act(k), x_ref, y_ref, psi_ref_i, k_profile, delta_r_profile);
-
+    [xproj_tl_t, yproj_tl_t, psi_ref_tl_t, k_trailer_t1_t, delta_r_tl_t, last_idx_tl] = projectRefPoint(x_t_t_act(k), y_t_t_act(k), x_ref, y_ref, psi_ref_i, k_profile, delta_r_profile);
+    
+    beta1_hist(k) = wrapToPi(psi_act(k) - psi_t_act(k));
     vr = v_profile(k); % vehicle speed (still taken from profile)
     % linearize using truck curvature for tractor part and trailer curvature for trailer feedforward
-    [A,B,W] = linearSys(vr, k_truck, delta_r_truck, psi_ref_tr, psi_ref_tl, m0, lr1, lt1, tau);
+    [A,B,W] = linearSys(vr, k_truck, k_trailer_t1_t, delta_r_truck, psi_ref_tr, psi_ref_tl, psi_ref_tl_t, m0, lr1, lt1, lt2, tau);
     [Ad, Bd, Cd, Wd] = tustin(A,B,W,dt);
 
     % Build prediction matrices Phi, Gamma, GammaW
@@ -195,14 +218,14 @@ for k = 1:Tsim
         % fallback to saturated LQR if QP fails
         disp('OSQP fail')
         warning('quadprog did not converge at step %d (flag=%d). Using saturated LQR fallback.', k, exitflag)
-        [A_lin, B_lin, W_lin] = linearSys(vr, kappa, delta_r, m0, lr1, lt1, tau);
+        [A_lin, B_lin, W_lin] = linearSys(vr, k_truck, k_trailer_t1_t, delta_r_truck, psi_ref_tr, psi_ref_tl, psi_t_t_ref, m0, lr1, lt1, tau);
         Kfb = lqr(A_lin,B_lin,Qx,Ru);
         u_dev = -Kfb * x_err;
         u_cmd = min(max(u_dev + delta_r, umin), umax);
         u_cmd_sat = u_cmd;
     else
         u_opt = zopt(1:nu);  % only use first control move
-        disp(u_opt)     
+%         disp(u_opt);     
         u_cmd = u_opt;
         u_cmd_sat = min(max(u_cmd, umin), umax);
         % assign previous cmd   
@@ -213,29 +236,37 @@ for k = 1:Tsim
     % propagate nonlinear states
     psi_dot = (vr * tan(delta_act(k))) / lr1;
     psi_t_dot = (vr/lt1)*sin(psi_act(k)-psi_t_act(k)) - (m0*vr/(lr1*lt1))*cos(psi_act(k)-psi_t_act(k))*tan(delta_act(k));
+    psi_t_t_dot = (vr/lt2)*sin(psi_t_act(k)-psi_t_t_act(k)) - (m0/lt2)*cos(psi_t_act(k)-psi_t_t_act(k))*psi_t_dot;
     delta_dot = -(delta_act(k) - u_cmd_sat) / tau;
 
     x_act(k+1) = x_act(k) + vr * cos(psi_act(k)) * dt;
     y_act(k+1) = y_act(k) + vr * sin(psi_act(k)) * dt;
     psi_act(k+1) = psi_act(k) + psi_dot * dt;
-    psi_t_act(k+1) = psi_t_act(k) + psi_t_dot * dt;
+    psi_t_act(k+1) = psi_t_act(k) + psi_t_dot * dt;             % yaw angle of trailer-1
+    psi_t_t_act(k+1) = psi_t_t_act(k) + psi_t_t_dot * dt;       % yaw angle of trailer-2
     delta_act(k+1) = delta_act(k) + delta_dot * dt;
     
     % calculate the trailer position
+    % trailer-1
     x_t_act(k+1) = x_act(k+1) - lr1*cos(psi_t_act(k+1)) - m0*cos(psi_act(k+1));
     y_t_act(k+1) = y_act(k+1) - lr1*sin(psi_t_act(k+1)) - m0*sin(psi_act(k+1));
+    % trailer-2
+    x_t_t_act(k+1) = x_t_act(k+1) - lt2*cos(psi_t_t_act(k+1)) - lt1*cos(psi_t_act(k+1));
+    y_t_t_act(k+1) = y_t_act(k+1) - lt2*sin(psi_t_t_act(k+1)) - lt1*sin(psi_t_act(k+1));
 
     % update error (Frenet) at next timestep
     % project new positions (tractor & trailer) onto reference
     [xproj_tr_p, yproj_tr_p, psi_ref_tr_p, ~, ~, last_idx_tr] = projectRefPoint(x_act(k+1), y_act(k+1), x_ref, y_ref, psi_ref_i, k_profile, delta_r_profile);
     [xproj_tl_p, yproj_tl_p, psi_ref_tl_p, ~, ~, last_idx_tl] = projectRefPoint(x_t_act(k+1), y_t_act(k+1), x_ref, y_ref, psi_ref_i, k_profile, delta_r_profile);
+    [         ~,          ~, psi_ref_t1_t_p, ~,~, last_idx_t1_t] = projectRefPoint(x_t_t_act(k+1), y_t_t_act(k+1), x_ref, y_ref, psi_ref_i, k_profile, delta_r_profile);
 
     dxp = x_act(k+1) - xproj_tr_p; dyp = y_act(k+1) - yproj_tr_p;
     e_y = -sin(psi_ref_tr_p)*dxp + cos(psi_ref_tr_p)*dyp;
     e_psi = wrapToPi(psi_act(k+1) - psi_ref_tr_p);
     e_psi_t = wrapToPi(psi_t_act(k+1) - psi_ref_tl_p);
+    e_psi_t_t = wrapToPi(psi_t_t_act(k+1) - psi_ref_t1_t_p);
 
-    x_err = [e_y; e_psi; e_psi_t; delta_act(k+1)];
+    x_err = [e_y; e_psi; e_psi_t; e_psi_t_t; delta_act(k+1)];
     history_err(:, k+1) = x_err;
 
 end
@@ -245,7 +276,7 @@ figure; hold on; axis equal; grid on;
 title('Tractor and trailer tracking animation'); 
 xlabel('x [m]'); ylabel('y [m]');
 
-% kích thước xe
+% vehicle parameters
 L_tr = 2.9; W_tr = 2.0;
 L_t  = 1.8; W_t = 2.0;
 
@@ -257,42 +288,71 @@ for k = 1:10:Tsim
     % plot trajectory 
     plot(x_act(1:k), y_act(1:k), 'b-', 'LineWidth', 1.2);
     plot(x_t_act(1:k), y_t_act(1:k), 'r-', 'LineWidth', 1.2);
+    plot(x_t_t_act(1:k), y_t_t_act(1:k), 'g-', 'LineWidth', 1.2);
 
     % tractor
     tractor_corners = rectangleCorners(x_act(k), y_act(k), psi_act(k), L_tr, W_tr);
     fill(tractor_corners(1,:), tractor_corners(2,:), 'b', 'FaceAlpha',0.3);
 
-    % trailer
+    % trailer-1
     trailer_corners = rectangleCorners(x_t_act(k), y_t_act(k), psi_t_act(k), L_t, W_t);
     fill(trailer_corners(1,:), trailer_corners(2,:), 'r', 'FaceAlpha',0.3);
-
-    % draw hitch line
+    
+    % trailer-2
+    trailer_corners = rectangleCorners(x_t_t_act(k), y_t_t_act(k), psi_t_t_act(k), L_t, W_t);
+    fill(trailer_corners(1,:), trailer_corners(2,:), 'g', 'FaceAlpha',0.3);
+    
+    % ---- Hitch connections ----
+    % Tractor rear axle hitch point
     hitch_tractor = [x_act(k) - m0*cos(psi_act(k));
                      y_act(k) - m0*sin(psi_act(k))];
-    hitch_trailer = [x_t_act(k) + lt1*cos(psi_t_act(k));
-                     y_t_act(k) + lt1*sin(psi_t_act(k))];
-    plot([hitch_tractor(1), hitch_trailer(1)], ...
-         [hitch_tractor(2), hitch_trailer(2)], 'k-', 'LineWidth',2);
 
-    legend('ref','tractor path','trailer path','tractor','trailer','hitch');
+    % Trailer-1 front axle hitch point
+    hitch_trailer1 = [x_t_act(k) + lbar*cos(psi_t_act(k));
+                      y_t_act(k) + lbar*sin(psi_t_act(k))];
+
+    % Draw tractor <-> trailer-1 connection
+    plot([hitch_tractor(1), hitch_trailer1(1)], ...
+         [hitch_tractor(2), hitch_trailer1(2)], 'k-', 'LineWidth', 2);
+
+    % Trailer-1 rear axle (where trailer-2 is attached)
+    rear_trailer1 = [x_t_act(k); y_t_act(k)];
+
+    % Trailer-2 front axle hitch point
+    hitch_trailer2 = [x_t_t_act(k) + lbar*cos(psi_t_t_act(k));
+                      y_t_t_act(k) + lbar*sin(psi_t_t_act(k))];
+
+    % Draw trailer-1 <-> trailer-2 connection
+    plot([rear_trailer1(1), hitch_trailer2(1)], ...
+         [rear_trailer1(2), hitch_trailer2(2)], 'k-', 'LineWidth', 1.5);
+
+ 
+%     legend('ref','tractor path','trailer path','tractor','trailer','hitch');
+    legend('ref','tractor path','trailer-1 path','trailer-2 path','tractor','trailer-1','trailer-2');
     drawnow;
 end
-
 %% Plot results
 figure('Units','normalized','Position',[0.05 0.05 0.9 0.8]);
 subplot(2,2,1);
 plot(x_ref, y_ref, 'k--','LineWidth',1.5); hold on; 
 plot(x_act,y_act,'b-','LineWidth',1.2); hold on; 
-plot(x_t_act, y_t_act, 'r-','LineWidth',1.5);
-title('Reference and Actual Trajectory'); xlabel('x [m]'); ylabel('y [m]'); axis equal; grid on; legend('ref','tractor','trailer');
+plot(x_t_act, y_t_act, 'g-.','LineWidth',1.5);
+% draw the second trailer
+plot(x_t_t_act, y_t_t_act, 'r-','LineWidth',1.5);
+title('Reference and Actual Trajectory'); xlabel('x [m]'); ylabel('y [m]'); axis equal; grid on; legend('ref','tractor','trailer-1','trailer-2');
 
 subplot(2,2,2);
 plot(t_ctrl, history_err(1,:), 'LineWidth',1.5);
 title('Tractor lateral error [m]'); xlabel('t [s]'); ylabel('y [m]'); grid on; legend('e_{y}');
 
 subplot(2,2,3);
-plot(t_ctrl, history_err(2,:), 'LineWidth',1.5); hold on; plot(t_ctrl, history_err(3,:),'--','LineWidth',1.5);
-title('Tractor and trailer yaw error [rad]'); xlabel('t [s]'); ylabel('rad'); grid on; legend('e_\psi','e_{\psi_t}');
+plot(t_ctrl, history_err(2,:), 'LineWidth',1.5); hold on;
+plot(t_ctrl, history_err(3,:),'--','LineWidth',1.5);
+% title('Tractor and trailer yaw error [rad]'); xlabel('t [s]'); ylabel('rad'); grid on; legend('e_\psi','e_{\psi_t}');
+plot(t_ctrl, history_err(4,:),'-.','LineWidth',1.5);
+title('Tractor and Trailer Yaw Error [rad]'); 
+xlabel('t [s]'); ylabel('rad'); grid on;
+legend('e_\psi','e_{\psi_t}','e_{\psi_{t2}}');
 
 subplot(2,2,4);
 hold on
@@ -301,31 +361,55 @@ plot(t_ctrl(1:end-1), umax*ones(size(t_ctrl(1:end-1))), 'r--', 'LineWidth', 1.5)
 plot(t_ctrl(1:end-1), umin*ones(size(t_ctrl(1:end-1))), 'r--', 'LineWidth', 1.5);
 title('Steering control command [rad]'); xlabel('t [s]'); ylabel('rad'); legend('u_{cmd}','u_{max}','u_{min}'); grid on;
 
+figure;
+plot(t_ctrl(1:end-1), beta1_hist, 'b-', 'LineWidth', 1.5);
+xlabel('t [s]');
+ylabel('\phi_1 [rad]');
+title('Articulation Angle: Tractor - Trailer 1');
+grid on;
 %% --- helper functions ---
-function [A, B, W] = linearSys(vr, k, delta_r, psi_ref, psi_t_ref, m0, lr1, lt1, tau)
+function [A, B, W] = linearSys(vr, k, k_t, delta_r, psi_ref, psi_t_ref, psi_t_t_ref, m0, lr1, lt1, lt2, tau)
     % util variables
     cos_delta_squared = cos(delta_r)^2;
     delta_yaw = psi_ref - psi_t_ref;
+    delta_yaw_t12 = psi_t_ref - psi_t_t_ref;
+    psi_t_t_ref_dot = k_t * vr;
+    
+    % matrix terms
     e_psi_term = (m0/lr1)*sin(delta_yaw)*tan(delta_r);
     e1 = (2*vr/lt1)*(cos(delta_yaw) + e_psi_term);
     e2 = -e1;
     
-    A = [0, vr    , 0      , 0;
-         0, 0     , 0      , vr/(lr1*cos_delta_squared);
-         0, e1    , e2    , -m0*vr*cos(delta_yaw)/(lr1*lt1*cos_delta_squared);
-         0, 0     , 0      , -1/tau];
-    B = [0;0;0;1/tau];
+%     A = [0, vr    , 0      , 0;
+%          0, 0     , 0      , vr/(lr1*cos_delta_squared);
+
+%          0, e1    , e2    , -m0*vr*cos(delta_yaw)/(lr1*lt1*cos_delta_squared);
+%          0, 0     , 0      , -1/tau];
+    e3 = (2*vr/lt1)*cos(delta_yaw_t12) + (2*m0/lt2)*sin(delta_yaw_t12)*psi_t_t_ref_dot;
+    e4 = (vr*m0*m0*cos(delta_yaw)*cos(delta_yaw_t12))/(lt2*lt1*lr1*cos_delta_squared);
+    % extend system with one more trailer
+    A = [0, vr    , 0      , 0   , 0                                                ;
+         0, 0     , 0      , 0   , vr/(lr1*cos_delta_squared)                       ;
+         0, e1    , e2     , 0   , -m0*vr*cos(delta_yaw)/(lr1*lt1*cos_delta_squared);
+         0, 0     , e3     ,-e3  , e4                                               ;
+         0, 0     , 0      , 0   ,-1/tau                                           ];
+    B = [0;0;0;0;1/tau];
     w1 = (vr/lr1)*(tan(delta_r) - delta_r/cos_delta_squared);
-    W = [0; -k*vr + w1; 0; 0];
+    W = [0; -k*vr + w1; 0; 0; 0];
 end
 
 function [Ad, Bd, Cd, Wd] = tustin(A,B,W,dt)
     I = eye(size(A));
     Ad = (I - 0.5*dt*A) \ (I + 0.5*dt*A);
     Bd = (I - 0.5*dt*A) \ (B * dt);
-    Cd = [1, 0, 0, 0;
-          0, 1, 0, 0;
-          0, 0, 1, 0];
+%     Cd = [1, 0, 0, 0;
+%           0, 1, 0, 0;
+%           0, 0, 1, 0];
+    % extend one trailer
+    Cd = [1, 0, 0, 0, 0;
+          0, 1, 0, 0, 0;
+          0, 0, 1, 0, 0;
+          0, 0, 0, 1, 0];
     Wd = (I - 0.5*dt*A) \ (W * dt);
 end
 
